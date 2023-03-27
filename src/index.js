4112d857f57e18fed269875a75f68409add3ba6d
Juan de Leon <jdele185@stu.southtexascollege.edu>
2023-03-27 17:05:30 -0500

    Vid Corr

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import cors from 'cors'
 import helmet from 'helmet'
 
 import logger from './helpers/logger'
+import router from './routes'
 
 const port = parseInt(process.env.PORT, 10) || 3000
 
@@ -15,11 +16,7 @@ app.use(morgan(process.env.MORGAN_LOG))
 app.use(cors({ origin: process.env.ORIGIN }))
 app.use(helmet())
 
-app.get('/', (req, res) => {
-  logger.info('Inside the root path')
-  const title = process.env.TITLE || 'Server'
-  res.send({ msg: title })
-})
+app.use(router)
 
 app.listen(port, () =>
   logger.info('Application started at http://localhost:${process.env.PORT'),
