645f2c6111fc79c0f8d907fbe12f48ba2f00845f
Juan de Leon <jdele185@stu.southtexascollege.edu>
2023-04-21 19:57:17 -0500

    Added Depts

--- src/models/companies.js
+++ src/models/companies.js
@@ -5,6 +5,9 @@ export const getCompanies = async () => db.company.findMany()
 export const getCompany = async (id) => 
   db.company.findUnique({ 
     where: { companyId: id },
+    include: {
+      Department: true,
+    },
 })
 
 export const addCompany = async (name) => db.company.create({ data: { name } })
